8bdbec2dbf596220ab2ab9e5aa108b9d9afde277
Piyushmeena11 <meenapiyush23@gmail.com>
2024-07-02 09:21:44 +0530

    Update progress.py

--- ggn/assets/progress.py
+++ ggn/assets/progress.py
@@ -5,8 +5,8 @@ import json
 from ggn.assets.functions import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "🟢"
-UN_FINISHED_PROGRESS_STR = "🔴"
+FINISHED_PROGRESS_STR = "🚣"
+UN_FINISHED_PROGRESS_STR = "🌊"
 DOWNLOAD_LOCATION = "/app"
 
 
